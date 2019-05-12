a9112c37fb33924d2071f8d8213ff54790b809bf
Phil Head <soultoyo@gmail.com>
2019-05-12 15:57:03 +0100

    Added mobile user agent detection

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+    // Take the user to a different screen here.
+    location.href="mobile.html"
+}
